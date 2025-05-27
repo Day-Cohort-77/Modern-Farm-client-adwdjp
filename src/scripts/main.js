d90f5dee44e051e0607ba19b5a550363da56822c
Stephen <peterson.stephen138@gmail.com>
2025-05-27 11:49:14 -0500

    added seed directory

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,14 @@
+<<<<<<< Updated upstream
 import { createPlan } from "./plan.js";
 console.log("Welcome to the main module");
 //import and invoke createPlan() from plan.js module
 const yearlyPlan = createPlan();
+=======
+import { createPlan } from "./plan.js"
+
+const yearlyPlan = createPlan()
+
+console.log("Welcome to the main module")
+
+console.log(yearlyPlan)
+>>>>>>> Stashed changes
