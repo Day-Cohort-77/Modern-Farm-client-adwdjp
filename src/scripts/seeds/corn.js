e21ac0ce784a6122693922d788f82675e925ca03
Dylan Johnson <dylanjohnson263@gmail.com>
2025-05-28 10:22:47 -0500

    Added corn function

---
+++ src/scripts/seeds/corn.js
@@ -0,0 +1,20 @@
+// define and export a function  for creating a corn seed
+// named createCorn. do not use the function keyword use arrow 
+// function syntax. the function should return an object with the
+// following properties: type, height, output. 
+// the createCorn function should return an array with 
+// two identical objects in it.
+export const createCorn = () => {
+   return [ {
+        type: "corn",
+        height: 180,
+        output: 6
+    },
+    {
+        type: "corn",
+        height: 180,
+        output: 6
+},
+]
+}
+
